6605c91f6f58b3a451efdf8d0d898e64e3137060
kamiduki <kamiduki_0@outlook.com>
2024-03-01 17:28:34 +0800

    add ctor for defer, make whose filed private, delete whose copy ctor and copy assign function

--- include/defer.h
+++ include/defer.h
@@ -7,9 +7,12 @@ namespace __details {
 template <typename _Func>
 struct [[nodiscard]] defer {
   using function_type = _Func;
-  // static_assert(std::is_nothrow_invocable_v<function_type>);
-  // TODO: make this field private
+private:
   function_type _M_f;
+public:
+  defer(_Func func) noexcept : _M_f(func) {}
+  defer(defer const&) = delete;
+  auto operator=(defer const&) -> defer& = delete;
 #if __cplusplus >= 202002L
   constexpr 
 #endif
